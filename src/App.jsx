9d07c8608d36c1721dd749dbe7253859426dbee0
Minglun Shao <dev.minglunshao@gmail.com>
2023-11-25 16:37:12 -0500

    add not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from "./pages/Home"
-import Layout from "./pages/Layout"
-import Projects from "./pages/Projects"
-import TechStack from "./pages/TechStack"
-import Contact from "./pages/Contact"
+import Home from './pages/Home';
+import Layout from './pages/Layout';
+import Projects from './pages/Projects';
+import TechStack from './pages/TechStack';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 function App() {
-
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout/>}>
-                    <Route index element={<Home />} />
-                    <Route path="/projects" element={<Projects/>} />
-                    <Route path="/tech-stack" element={<TechStack/>} />
-                    <Route path="/contact" element={<Contact/>} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					<Route path='*' element={<NotFound />} />
+					<Route index element={<Home />} />
+					<Route path='/projects' element={<Projects />} />
+					<Route path='/tech-stack' element={<TechStack />} />
+					<Route path='/contact' element={<Contact />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+	return (
+		<div class='flex items-center justify-center w-screen h-screen text-port-light flex-col gap-2'>
+			<h1 class=' text-6xl'>This page does not exist</h1>
+		</div>
+	);
+};
+
+export default NotFound;
