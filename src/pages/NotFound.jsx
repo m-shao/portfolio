cab536b73b5e3cbc4bd7b03613661b5279c3662e
Minglun Shao <dev.minglunshao@gmail.com>
2023-11-25 16:40:58 -0500

    add not found page to router

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const NotFound = () => {
 	return (
 		<div class='flex items-center justify-center w-screen h-screen text-port-light flex-col gap-2'>
-			<h1 class=' text-6xl'>This page does not exist</h1>
+			<h1 class=' text-7xl pb-12 px-12 text-center'>
+				This page does not exist
+			</h1>
 		</div>
 	);
 };
